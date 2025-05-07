5e0e88d42d79b88262e6c70a150d5ddf4364f12e
Orionsprod <138870673+Orionsprod@users.noreply.github.com>
2025-05-07 19:31:53 +0300

    Delete config.ts

--- config.ts
+++
@@ -1,6 +0,0 @@
-export const NOTION_TOKEN = Deno.env.get("NOTION_TOKEN")!;
-export const GOOGLE_ROOT_FOLDER_ID = Deno.env.get("GOOGLE_ROOT_FOLDER_ID")!;
-export const SERVICE_ACCOUNT_JSON = Deno.env.get("GOOGLE_SERVICE_ACCOUNT_JSON")!;
-export const ROOT_PROJECTS_ID = Deno.env.get("ROOT_PROJECTS_ID")!;
-export const ROOT_ARCHIVES_ID = Deno.env.get("ROOT_ARCHIVES_ID")!;
-export const DEBUG = Deno.env.get("DEBUG") === "true";
